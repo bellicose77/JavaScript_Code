ebd3c51948e1377f829b7b2d95d9dc657c30ea66
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-03-26 22:41:44 +0600

    type checked

--- typeconversion.js
+++ typeconversion.js
@@ -1,6 +1,7 @@
 let value1 = 5;
-let value2 = '9'
-let sum = value1+value2;
+let value2 = '9';
+let value3 = 4;
+let sum = value1+value3+value2;
 let sum1 = value2 + value1;
 console.log(sum)
 console.log(sum1)
